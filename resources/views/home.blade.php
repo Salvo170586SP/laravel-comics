b88e5cb4b99f76feaedf29e3abc767dedcb85512
Salvo170586SP <pitanza.salvatore.86@gmail.com>
2022-03-15 17:10:18 +0100

    added load more

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -47,11 +47,10 @@
                     <h3>{{ $card['series'] }}</h3>
                 </div>
                 @empty
-                    <h3>NON CI SONO COMICS DISPONIBILI</h3>
+                <h3>NON CI SONO COMICS DISPONIBILI</h3>
                 @endforelse
 
-
-
+                    <a href="#">LOAD MORE</a>
 
             </div>
         </section>
@@ -97,12 +96,48 @@
     <footer>
         <div class="footer-top">
             <div class="container">
+                <div class="menu-site">
+                    <div class="left-menu">
+                        <h3>DC COMICS</h3>
+                        <ul>
+                            <li><a href="#">Caractsers</a></li>
+                            <li><a href="#">Comics</a></li>
+                            <li><a href="#">Movies</a></li>
+                            <li><a href="#">TV</a></li>
+                            <li><a href="#">Games</a></li>
+                            <li><a href="#">Videos</a></li>
+                            <li><a href="#">News</a></li>
+                        </ul>
+                        <h3>SHOP</h3>
+                        <ul>
+                            <li><a href="#">Shop DC</a></li>
+                            <li><a href="#">Shop DC Collectibles</a></li>
+                        </ul>
+                    </div>
+
+                    <div class="right-menu">
+                        <h3>DC</h3>
+                        <ul>
+                            <li><a href="#">Caractsers</a></li>
+                            <li><a href="#">Comics</a></li>
+                            <li><a href="#">Movies</a></li>
+                            <li><a href="#">TV</a></li>
+                            <li><a href="#">Games</a></li>
+                            <li><a href="#">Videos</a></li>
+                            <li><a href="#">News</a></li>
+                        </ul>
+                    </div>
+
+                </div>
+
+
+
 
             </div>
         </div>
         <div class="footer-bottom">
             <div class="container d-flex">
-                 <a href="#">SIGN-UP NOW</a>
+                <a href="#">SIGN-UP NOW</a>
                 <div class="social">
                     <h4>FOLLOW US</h4>
                     <figure>
