d6ce386702fce534b6fe410beb747d56e525d85c
Salvo170586SP <pitanza.salvatore.86@gmail.com>
2022-03-15 17:33:11 +0100

    added header blade include

---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,25 @@
+
+<div class="container">
+        <header>
+            <figure>
+                <img src="{{ asset('img/dc-logo.png')}}" alt="logo">
+            </figure>
+            <nav>
+                <ul>
+                    <li><a href="#">CHARACTERS</a></li>
+                    <li><a href="#">COMICS</a></li>
+                    <li><a href="#">MOVIES</a></li>
+                    <li><a href="#">TV</a></li>
+                    <li><a href="#">GAMES</a></li>
+                    <li><a href="#">COLLECTIBLES</a></li>
+                    <li><a href="#">VIDEOS</a></li>
+                    <li><a href="#">FANS</a></li>
+                    <li><a href="#">NEWS</a></li>
+                    <li><a href="#">SHOP</a></li>
+                </ul>
+            </nav>
+            <div class="search">
+                <input type="text" placeholder="Search" >
+            </div>
+        </header>
+    </div>
