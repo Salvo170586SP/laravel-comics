ea74a93c4dcbd00c278e691ad52de738773e0426
Salvo170586SP <pitanza.salvatore.86@gmail.com>
2022-03-15 15:30:05 +0100

    added header and jumbotron

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,20 +1,47 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Disc Comics</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
+
 <body>
-    <header>
-        
-    </header>
-    <main>
+    <div class="container">
+        <header>
+            <figure>
+                <img src="{{ asset('img/dc-logo.png')}}" alt="logo">
+            </figure>
+            <nav>
+                <ul>
+                    <li><a href="#">CHARACTERS</a></li>
+                    <li><a href="#">COMICS</a></li>
+                    <li><a href="#">MOVIES</a></li>
+                    <li><a href="#">TV</a></li>
+                    <li><a href="#">GAMES</a></li>
+                    <li><a href="#">COLLECTIBLES</a></li>
+                    <li><a href="#">VIDEOS</a></li>
+                    <li><a href="#">FANS</a></li>
+                    <li><a href="#">NEWS</a></li>
+                    <li><a href="#">SHOP</a></li>
+                </ul>
+            </nav>
+        </header>
+    </div>
 
-    </main>
-    <footer>
+        <main>
+            <section id="jumbotron">
 
-    </footer>
+            </section>
+
+        </main>
+
+        <footer>
+
+        </footer>
 </body>
+
 </html>
