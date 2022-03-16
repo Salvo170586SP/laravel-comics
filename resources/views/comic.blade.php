10a27bb9fad2ec06a31ac08c1481cab926eda25b
Salvo170586SP <pitanza.salvatore.86@gmail.com>
2022-03-16 16:04:51 +0100

    added buy layout

--- resources/views/comic.blade.php
+++ resources/views/comic.blade.php
@@ -9,11 +9,25 @@
 <section id="comic">
     <div class="container">
         <div class="box-comic">
+
             <div class="info-comic">
                 <h2>ACTION COMICS #1000</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus expedita perferendis, quas earum corrupti animi quae tempore distinctio obcaecati ea odio recusandae placeat, in laboriosam rerum? Inventore rerum sit necessitatibus.</p>
+                
+                <div class="buy">
+                    <div class="price"> 
+                        <span>U.S Price:</span>
+                        <span>Available</span>
+                    </div>
+                    <div class="check"> Check Availability</div>                    
+                </div>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus expedita perferendis, quas earum corrupti animi quae tempore distinctio obcaecati ea odio recusandae placeat, in laboriosam rerum? Inventore rerum sit necessitatibus.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus expedita perferendis, quas earum corrupti animi quae tempore distinctio obcaecati ea odio recusandae placeat, in laboriosam rerum? Inventore rerum sit necessitatibus.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus expedita perferendis, quas earum corrupti animi quae tempore distinctio obcaecati ea odio recusandae placeat, in laboriosam rerum? Inventore rerum sit necessitatibus.
+                </p>
             </div>
             <div class="img-comic">
+                <p>ADVERTISEMENT</p>
                 <figure>
                     <img src="{{ asset('img/adv.jpg') }}" alt="img">
                 </figure>
