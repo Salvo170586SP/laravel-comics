baf7240f2d658d4add27e0fd53bc3f96d1db1afe
Salvo170586SP <pitanza.salvatore.86@gmail.com>
2022-03-16 16:59:52 +0100

    added detail comics

--- resources/views/comic.blade.php
+++ resources/views/comic.blade.php
@@ -4,8 +4,8 @@
 @section('content')
 
 <section id="title-card">
-
 </section>
+
 <section id="comic">
     <div class="container">
         <div class="box-comic">
@@ -31,6 +31,38 @@
                 <figure>
                     <img src="{{ asset('img/adv.jpg') }}" alt="img">
                 </figure>
+            </div>        
+        </div>
+    </div>
+</section>
+<section id="detail-comic">
+    <div class="container">
+        <div class="d-flex">
+            <div class="talent">
+                <h4>Talent</h4>
+                <div class="talent-item">
+                    <span>Art by</span>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum officia accusamus id temporibus tempore quia est asperiores vitae odit, consectetur iure. Explicabo dicta, illum id incidunt enim veniam quia sunt?</p>
+                </div>
+                <div class="talent-item">
+                    <span>Written by</span>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum officia accusamus id temporibus tempore quia est asperiores vitae odit, consectetur iure. Explicabo dicta, illum id incidunt enim veniam quia sunt?</p>
+                </div>
+            </div>
+            <div class="specs">
+                <h4>Specs</h4>
+                <div class="specs-item">
+                    <span>Series: </span>
+                    <p>Action comics</p>
+                </div>
+                <div class="specs-item">
+                    <span>U.S Price: </span>
+                    <p>$19.99</p>
+                </div>
+                <div class="specs-item">
+                    <span>On Sale Date: </span>
+                    <p>Oct 02 2018</p>
+                </div>
             </div>
         </div>
     </div>
